4c2da9dc952ebedaf8a3b65218ad691f4f422403
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2026-05-05 16:30:59 -0500

    fixed MC version detection being too strict.

--- src/test/java/builderb0y/f3screenshot/TheTest.java
+++ src/test/java/builderb0y/f3screenshot/TheTest.java
@@ -1,5 +1,7 @@
 package builderb0y.f3screenshot;
 
+import org.junit.jupiter.api.Test;
+
 /**
 trick gradle into actually running the test task,
 even though there are no tests to run.
@@ -7,4 +9,6 @@ this ensures that my doFirst() {} block runs.
 */
 public class TheTest {
 
+	@Test
+	void theTest() {}
 }
